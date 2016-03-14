16a9b7dd6468e4d5f15626c4bb10a7862522b5c0
rabiayorganci <rabiayorganci@gmail.com>
2016-03-15 01:21:07 +0200

    main page edit

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import redirect
 # Create your views here.
 
 def post_list(request):
-    posts = Post.objects.filter(yayinlanma_tarihi__lte=timezone.now()).order_by('yayinlanma_tarihi')
+    posts = Post.objects.filter(yayinlanma_tarihi__lte=timezone.now()).order_by('-yayinlanma_tarihi')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):

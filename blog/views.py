a158f55f8ded1fb81b33e72bed3c24878fc09239
rabiayorganci <rabiayorganci@gmail.com>
2016-03-15 02:53:41 +0200

    add gallery

--- blog/views.py
+++ blog/views.py
@@ -4,11 +4,17 @@ from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
+import os
+from django.conf import settings
+
 # Create your views here.
 
 def post_list(request):
     posts = Post.objects.filter(yayinlanma_tarihi__lte=timezone.now()).order_by('-yayinlanma_tarihi')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    #return render(request, 'blog/post_list.html', {'posts': posts})
+    path = "static/img/gallery/"  
+    img_list =os.listdir(path)
+    return render(request, 'blog/post_list.html', {'images': img_list, 'posts': posts})
 
 def post_detail(request, pk):
 	post = get_object_or_404(Post, pk=pk)
